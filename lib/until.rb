cf063148482ed4c755e1d2715253cbd607844c07
Terry Threatt <terry.threatt@gmail.com>
2020-02-14 00:05:21 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,7 +1,7 @@
 def using_until
   levitation_force = 6
   #your code here
-  until levitation_force > 10 
+  until levitation_force >= 10 
     looping_string = "Wingardium Leviosa"
     puts looping_string
     levitation_force += 1
